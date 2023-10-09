b54fe38167bfe260e8b7dd177b999169466df0b2
iStephen27 <alexander.stephen@outlook.es>
2023-10-08 22:52:14 -0600

    WebDev Homework

---
+++ src/test/java/Homework/WebDevelopment/WebDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package Homework.WebDevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
